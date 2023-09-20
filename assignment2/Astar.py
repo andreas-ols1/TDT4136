5ca9e8b701a5b13245230c4ac901112b74030c52
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2023-09-20 18:20:46 +0200

    Add documentation in code

--- assignment2/Astar.py
+++ assignment2/Astar.py
@@ -6,15 +6,28 @@ from typing import Callable
 
 def h(current: list[int], goal: list[int]) -> int:
     """
-    Heuristic function for A* algorithm
-    Takes two positional arguments with list
-    of coordinates and returns Manhattan distance
+    Heuristic function for A* algorithm.
+
+    Args:
+        current (list[int]): The current node's coordinates [x, y].
+        goal (list[int]): The goal node's coordinates [x, y].
+
+    Returns:
+        int: The Manhattan distance between the current node and the goal node.
     """
     return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
 
 def init_cells(a: Map_Obj) -> list[Cell]:
-    """Initializes a 2D list of cells from a Map_Obj"""
+    """
+    Initializes a 2D list of cells from a Map_Obj.
+
+    Args:
+        a (Map_Obj): The map object containing map data.
+
+    Returns:
+        list[Cell]: A 2D list of initialized Cell objects.
+    """
     cells1 = []
     for i in range(len(a.get_maps()[0])):
         cells2 = []
@@ -24,7 +37,16 @@ def init_cells(a: Map_Obj) -> list[Cell]:
     return cells1
 
 
-def reconstruct_path(current: Cell):
+def reconstruct_path(current: Cell) -> list[Cell]:
+    """
+    Reconstructs the path from the current cell to the start cell.
+
+    Args:
+        current (Cell): The current cell.
+
+    Returns:
+        list[Cell]: A list of cells representing the path from start to current cell.
+    """
     path = [current]
     while current.parent:
         current = current.parent
@@ -32,22 +54,45 @@ def reconstruct_path(current: Cell):
     return path
 
 
-def attach_parent(child: Cell, parent: Cell, goal: list[int]):
+def attach_parent(child: Cell, parent: Cell, goal: list[int]) -> None:
+    """
+    Attaches a parent cell to a child cell, and updates the child's g, h, and f values.
+
+    Args:
+        child (Cell): The child cell.
+        parent (Cell): The parent cell.
+        goal (list[int]): The goal node's coordinates [x, y].
+    """
     child.parent = parent
     child.update_g()
     child.update_h(h, goal)
     child.update_f()
 
 
-def improve_path(cell: Cell):
+def improve_path(cell: Cell) -> None:
+    """
+    Improves the path for a given cell by considering its children.
+
+    Args:
+        cell (Cell): The cell for which to improve the path.
+    """
     for child in cell.get_children():
-        if cell.g + cell.get_cost() < child.g:
+        if cell.g + cell.get_cost() < child.get_g():
             child.parent = cell
             child.update_g()
 
 
-def A_star(a: Map_Obj, h: Callable):
-    """A* algorithm: Returns the shortest path from start to goal if it exists, otherwise false"""
+def A_star(a: Map_Obj, h: Callable) -> list[Cell] | bool:
+    """
+    A* algorithm: Returns the shortest path from start to goal if it exists, otherwise False.
+
+    Args:
+        a (Map_Obj): The map object containing map data.
+        h (Callable): The heuristic function to estimate the cost from a node to the goal.
+
+    Returns:
+        list[Cell] | bool: A list of cells representing the shortest path or False if no path is found.
+    """
     all_cells = init_cells(a)
     frontier = Min_Cell_Heap()
     visited = []
@@ -58,16 +103,19 @@ def A_star(a: Map_Obj, h: Callable):
         current_cell = frontier.extract_min()
         visited.append(current_cell)
 
+        # Return the path if the current cell is the goal cell
         if current_cell.get_position() == a.get_goal_pos():
             return reconstruct_path(current_cell)
 
         current_cell.generate_children(all_cells)
 
         for child in current_cell.get_children():
+            # If the child is not in the visited list or the frontier, add it to the frontier
             if child not in visited and child not in frontier:
                 attach_parent(child, current_cell, a.get_goal_pos())
                 frontier.insert(child)
-            elif current_cell.g + current_cell.get_cost() < child.g:
+            # If the child is in the frontier and the current path is better than the previous path, update the child
+            elif current_cell.g + current_cell.get_cost() < child.get_g():
                 attach_parent(child, current_cell, a.get_goal_pos())
                 if child in visited:
                     improve_path(child)
@@ -76,6 +124,7 @@ def A_star(a: Map_Obj, h: Callable):
 
 
 if __name__ == "__main__":
-    a = Map_Obj(task=1)
+    a = Map_Obj(task=4)
     shortest_path = A_star(a, h)
-    a.show_map(path=shortest_path)
+    a.save_map(path=shortest_path, location="visualisations/task4.png")
+    # a.show_map(path=shortest_path)
--- assignment2/Cell.py
+++ assignment2/Cell.py
@@ -1,4 +1,30 @@
 class Cell:
+    """
+    A class representing a cell in a grid for pathfinding algorithms.
+
+    Attributes:
+        position (tuple): The (x, y) coordinates of the cell.
+        parent (Cell): The parent cell from which this cell is reached in the path.
+        cost (int, optional): The cost associated with moving to this cell (default is -1).
+        f (int): The total cost of the cell (g + h).
+        g (int): The cost from the start node to this cell.
+        h (int): The heuristic cost from this cell to the goal.
+        children (list): List of neighboring cells that can be reached from this cell.
+
+    Methods:
+        generate_children(self, cells):
+            Adds neighboring cells to the children list if they are not walls.
+
+        update_h(self, h, goal):
+            Updates the heuristic cost (h) of the cell based on a given heuristic function and a goal cell.
+
+        update_g(self):
+            Recalculates the cost from the start node to this cell (g) based on the parent's g value and the cell's cost.
+
+        update_f(self):
+            Recalculates the total cost of the cell (f) based on the updated g and h values.
+    """
+
     def __init__(self, position, parent, cost=-1):
         self.position = position
         self.f = 0
@@ -19,12 +45,6 @@ class Cell:
 
     def __gt__(self, other):
         return self.f > other.f
-
-    def __ge__(self, other):
-        return self.f >= other.f
-
-    def __le__(self, other):
-        return self.f <= other.f
 
     def get_position(self):
         return self.position
@@ -48,6 +68,7 @@ class Cell:
         return self.parent
 
     def generate_children(self, cells):
+        """Adds neighbours to the children list if they are not walls"""
         neighbours = []
         neighbours.append(cells[self.position[0] - 1][self.position[1]])
         neighbours.append(cells[self.position[0] + 1][self.position[1]])
